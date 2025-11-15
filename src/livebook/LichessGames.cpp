39b9c379b68a2937a799d1194a44a7e60726a6e0
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-11-15 16:47:55 +0100

    Delete src/livebook directory

--- src/livebook/LichessGames.cpp
+++
@@ -1,8 +0,0 @@
-#ifdef USE_LIVEBOOK
-    #include "LichessGames.h"
-
-using namespace Stockfish::Livebook;
-LichessGames::LichessGames() :
-    LichessOpening("https://explorer.lichess.ovh/lichess?") {}
-
-#endif
--- src/livebook/LichessMaster.cpp
+++
@@ -1,8 +0,0 @@
-#ifdef USE_LIVEBOOK
-    #include "LichessMaster.h"
-
-using namespace Stockfish::Livebook;
-
-LichessMaster::LichessMaster() :
-    LichessOpening("https://explorer.lichess.ovh/masters?") {}
-#endif
