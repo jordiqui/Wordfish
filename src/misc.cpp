6b96d7520fedc1e7657aa812070879909fb93f75
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-11-21 12:12:07 +0100

    Rebrand to Wordfish-3.50

--- src/misc.cpp
+++ src/misc.cpp
@@ -40,7 +40,7 @@ namespace Stockfish {
 namespace {
 
 // Engine branding information.
-constexpr std::string_view engine_name = "Wordfish-3.40-181125";
+constexpr std::string_view engine_name = "Wordfish-3.50";
 constexpr std::string_view engine_author_name =
   "Jorge Ruiz and the Stockfish developers (see AUTHORS file)";
 constexpr std::string_view engine_author_prefix = "Developed by ";
