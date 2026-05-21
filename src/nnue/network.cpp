5073733a167073cf4ebe22c6ed06b57d16d35ca0
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2026-05-21 20:16:23 +0200

    Retire NetworkSmall type surface

--- src/nnue/network.cpp
+++ src/nnue/network.cpp
@@ -413,7 +413,5 @@ bool Network<Arch, Transformer>::write_parameters(std::ostream&      stream,
 template class Network<NetworkArchitecture<TransformedFeatureDimensionsBig, L2Big, L3Big>,
                        FeatureTransformer<TransformedFeatureDimensionsBig>>;
 
-template class Network<NetworkArchitecture<TransformedFeatureDimensionsSmall, L2Small, L3Small>,
-                       FeatureTransformer<TransformedFeatureDimensionsSmall>>;
 
 }  // namespace Stockfish::Eval::NNUE
