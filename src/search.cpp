373b2160e14a6a0e93906492b049ff01fefac057
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2026-06-11 09:22:11 +0200

    P2-SF20260606: simplify SEE margin formula

--- src/search.cpp
+++ src/search.cpp
@@ -1284,7 +1284,7 @@ moves_loop:  // When in check, search starts here
 
                 // SEE based pruning for captures and checks
                 // Avoid pruning sacrifices of our last piece for stalemate
-                int margin = std::max(157 * depth + captHist / 29, 0);
+                int margin = 157 * depth + captHist / 29;
                 if ((alpha >= VALUE_DRAW || pos.non_pawn_material(us) != PieceValue[movedPiece])
                     && !pos.see_ge(move, -margin))
                     continue;
