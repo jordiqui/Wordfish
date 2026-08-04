5b421ff259585bbd49b55b27baac9a53493c88fc
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2026-08-04 07:01:56 -0600

    WORDFISH-P8F: update main NNUE network to nn-af1339a6dea3

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,8 +33,8 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-1b6a82263149.nnue"
-#define EvalFileDefaultName "nn-1b6a82263149.nnue"
+#define EvalFileDefaultNameBig "nn-af1339a6dea3.nnue"
+#define EvalFileDefaultName "nn-af1339a6dea3.nnue"
 
 namespace NNUE {
 class Network;
